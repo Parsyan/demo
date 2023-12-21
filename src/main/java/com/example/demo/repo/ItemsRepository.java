1ff36b28f2691125bc95200e2d152d373b67346f
Gor <gorparsyan13@mail.ru>
2023-12-21 10:40:06 +0400

    4main

--- src/main/java/com/example/demo/repo/ItemsRepository.java
+++ src/main/java/com/example/demo/repo/ItemsRepository.java
@@ -4,6 +4,9 @@ import com.example.demo.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ItemsRepository extends JpaRepository<Item, Long> {
+    Optional<Item> existsItemByName(String name);
 }
--- src/main/java/com/example/demo/service/ItemService.java
+++ src/main/java/com/example/demo/service/ItemService.java
@@ -1,9 +1,12 @@
 package com.example.demo.service;
 
+import com.example.demo.exception.UnsupportedUserInDBException;
 import com.example.demo.model.Item;
 import com.example.demo.model.Person;
 import com.example.demo.repo.ItemsRepository;
 import com.example.demo.repo.PeopleRepository;
+import jakarta.persistence.EntityExistsException;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +17,21 @@ public class ItemService {
     private final ItemsRepository itemsRepository;
     private final PeopleRepository peopleRepository;
 
+    @Transactional
     public void registerItem(Item item, Person person){
+        var existsed = itemsRepository.existsItemByName(item.getName());
+        if (existsed.isPresent()) {
+            throw new EntityExistsException("Item is Exists");
+        }
 
+        item.setPerson(person);
+        itemsRepository.save(item);
+    }
+    @Transactional
+    public void registerItem(Item item) throws UnsupportedUserInDBException {
+        if(item.getPerson() == null)
+            throw new UnsupportedUserInDBException("User Not Found");
+
+        itemsRepository.save(item);
     }
 }
